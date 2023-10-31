47ad5818ce12a21cf88c0ba15a13f5b2e19bf9cc
Pedro.H.Cavalcante <92176929+Lushlitz@users.noreply.github.com>
2023-10-31 16:14:06 -0300

    Add files via upload

---
+++ sistema.py
@@ -0,0 +1,15 @@
+from viagem import Viagem
+print("=========================")
+print("FAÇA SUA ESCOLHA:")
+print("1 - Aracaju - 80$$")
+print("2 - Salvador - 90$")
+print("3 - Recife - 115$")
+print("4 - Petrolândia - 50$")
+print("5 - Senhor do bomfim - 90$")
+print("6 - Garanhuns - 100$")
+print("7 - Serra Talhada - 80$")
+print("8 - Floresta - 85$")
+print("9 - Delmiro Golveia 40$")
+print("=========================")
+
+escolha = int(input("Digite o número do seu destino"))
---
+++ viagem.py
@@ -0,0 +1,14 @@
+class Viagem:
+    def __init__(self, preco, tempo):
+        self.preco = preco
+        self.tempo = tempo
+
+Aracaju = Viagem(preco= 80, tempo= 5)
+Salvador = Viagem(preco= 90, tempo=8)
+Recife = Viagem(preco= 115, tempo=9)
+Petrolandia = Viagem(preco= 50, tempo=2)
+Senhor_do_Bonfim = Viagem(preco=90, tempo= 5)
+Garanhuns = Viagem(preco=100, tempo=6)
+Serra_Talhada = Viagem(preco=80, tempo=5)
+Floresta = Viagem(preco=85, tempo=4)
+Delmiro_Golveia = Viagem(preco=40, tempo=1.30)
