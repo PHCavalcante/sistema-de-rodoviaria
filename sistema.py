148d6b6b298651e327c41932b828b242fcfc5a7e
Pedro.H.Cavalcante <92176929+Lushlitz@users.noreply.github.com>
2023-10-31 19:42:45 -0300

    Add files via upload

--- sistema.py
+++ sistema.py
@@ -1,4 +1,5 @@
 from viagem import Viagem
+
 print("=========================")
 print("FAÇA SUA ESCOLHA:")
 print("1 - Aracaju - 80$$")
@@ -10,6 +11,9 @@ print("6 - Garanhuns - 100$")
 print("7 - Serra Talhada - 80$")
 print("8 - Floresta - 85$")
 print("9 - Delmiro Golveia 40$")
+print(" 10 - Arapiraca - 70$")
 print("=========================")
 
 escolha = int(input("Digite o número do seu destino"))
+
+sua_viagem()
--- viagem.py
+++ viagem.py
@@ -1,14 +1,22 @@
 class Viagem:
-    def __init__(self, preco, tempo):
+    def __init__(self, preco, tempo, distancia, destino):
         self.preco = preco
         self.tempo = tempo
-
-Aracaju = Viagem(preco= 80, tempo= 5)
-Salvador = Viagem(preco= 90, tempo=8)
-Recife = Viagem(preco= 115, tempo=9)
-Petrolandia = Viagem(preco= 50, tempo=2)
-Senhor_do_Bonfim = Viagem(preco=90, tempo= 5)
-Garanhuns = Viagem(preco=100, tempo=6)
-Serra_Talhada = Viagem(preco=80, tempo=5)
-Floresta = Viagem(preco=85, tempo=4)
-Delmiro_Golveia = Viagem(preco=40, tempo=1.30)
+        self.distancia = distancia
+        self.destino = destino
+    
+    def sua_viagem(self):
+        print("Você está viajando para", destino, "Distância da viagem". distancia, "Boa viagem!")
+    def valor_passagem(self):
+        print("Sua viagem para", destino, "custa", preco)
+        
+Aracaju = Viagem(preco= 80, tempo= 5, distancia= 281.1)
+Salvador = Viagem(preco= 90, tempo=8, distancia= 470.3)
+Recife = Viagem(preco= 115, tempo=9, distancia= 447.4)
+Petrolandia = Viagem(preco= 50, tempo=2, distancia= 63.4)
+Senhor_do_Bonfim = Viagem(preco=90, tempo= 5, distancia= 397.5)
+Garanhuns = Viagem(preco=100, tempo=6, distancia= 219.9)
+Serra_Talhada = Viagem(preco=80, tempo=5, distancia= 214.1)
+Floresta = Viagem(preco=85, tempo=4, distancia= 98.2)
+Delmiro_Golveia = Viagem(preco=40, tempo=1.30, distancia= 37.7)
+Arapiraca = Viagem(preco=70 , tempo=6, distancia= 203.6)
