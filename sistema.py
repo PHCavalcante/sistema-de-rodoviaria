21fd190e13feb5908facad7170934ec3d5adbfff
Pedro.H.Cavalcante <92176929+Lushlitz@users.noreply.github.com>
2023-11-06 20:06:23 -0300

    Add files via upload

--- sistema.py
+++ sistema.py
@@ -1,4 +1,8 @@
-from viagem import Viagem
+import os
+from viagem import Terminal
+from viagem import Viagem_Paulo_Afonso
+from viagem import Viagem_Demiro
+from viagem import Viagem_Gloria
 from viagem import Aracaju
 from viagem import Salvador
 from viagem import Recife
@@ -10,26 +14,63 @@ from viagem import Floresta
 from viagem import Delmiro_Golveia
 from viagem import Arapiraca
 
-print("=========================")
-print("FAÇA SUA ESCOLHA:")
-print("1 - Aracaju - 80$$")
-print("2 - Salvador - 90$")
-print("3 - Recife - 115$")
-print("4 - Petrolândia - 50$")
-print("5 - Senhor do bomfim - 90$")
-print("6 - Garanhuns - 100$")
-print("7 - Serra Talhada - 80$")
-print("8 - Floresta - 85$")
-print("9 - Delmiro Golveia 40$")
-print(" 10 - Arapiraca - 70$")
-print("=========================")
+def funcao_horario(hora):
+    print("Sua saída está marcada para as" + hora, "horas! não se atrase!")
 
-escolha = int(input("Digite o número do seu destino: "))
+while(True):
+    
+    print("================================")
+    print("ESCOLHA O TERMINAL QUE DESEJA: ")
+    print("1 - Terminal De Paulo Afonso - Bahia")
+    print("2 - Terminal de Glória - Bahia")
+    print("3 - Terminal de Delmiro - Alagoas")
+    print("=================================")
+
+    terminal = int(input("Digite o terminal desejado: "))
+    os.system('cls')
+    break
+    
+while(True):
+    print("=========================")
+    print("ESCOLHA O SEU DESTINO:")
+    print("1 - Aracaju - 80$$")
+    print("2 - Salvador - 90$")
+    print("3 - Recife - 115$")
+    print("4 - Petrolândia - 50$")
+    print("5 - Senhor do bomfim - 90$")
+    print("6 - Garanhuns - 100$")
+    print("7 - Serra Talhada - 80$")
+    print("8 - Floresta - 85$")
+    print("9 - Delmiro Golveia 40$")
+    print(" 10 - Arapiraca - 70$")
+    print("=========================")
+
+    escolha = int(input("Digite o número do seu destino: "))
+    os.system('cls')
+    break
+
+while(True):
+
+    print("===========================")
+    print("DIGITE O HORÁRIO DESEJADO: ")
+    print("1 - 07-00h")
+    print("2 - 10-00h")
+    print("3 - 13:00h")
+    print("4 - 15;00h")
+    print("===========================")
+
+    horario = int(input("Digite o horário de partida desejado: "))
+    os.system('cls')
+    break
 
 while (True):
     if escolha == 1:
         Aracaju.valor_passagem()
         Aracaju.sua_viagem()
+        if horario == 1:
+            funcao_horario(7)
+        elif horario == 2:
+            print("S")
         break
     elif escolha == 2:
         Salvador.valor_passagem()
--- viagem.py
+++ viagem.py
@@ -1,4 +1,4 @@
-class Viagem:
+class Viagem_Paulo_Afonso:
     def __init__(self, preco, tempo, distancia, destino):
         self.preco = preco
         self.tempo = tempo
@@ -10,13 +10,68 @@ class Viagem:
     def valor_passagem(self):
         print(f"Sua passagem para", self.destino, "custa", self.preco + "$")
 
-Aracaju = Viagem("80", "6", "281.1", "Aracaju")
-Salvador = Viagem("90", "8", "470.3", "Salvador")
-Recife = Viagem("115", "9", "447.4", "Recife")
-Petrolandia = Viagem("50", "2", "63.4", "Petrolândia")
-Senhor_do_Bonfim = Viagem("90", "5", "397.5", "Senhor do bomfim")
-Garanhuns = Viagem("100", "6", "219.9", "Garanhuns")
-Serra_Talhada = Viagem("80", "5", "214.1", "Serra Talhada")
-Floresta = Viagem("85", "4", "98.2", "Floresta")
-Delmiro_Golveia = Viagem("40", "1.30", "37.7", "Delmiro Golveia")
-Arapiraca = Viagem("70", "6", "203.6", "Arapiraca")
+Aracaju = Viagem_Paulo_Afonso("80", "6", "281.1", "Aracaju")
+Salvador = Viagem_Paulo_Afonso("90", "8", "470.3", "Salvador")
+Recife = Viagem_Paulo_Afonso("115", "9", "447.4", "Recife")
+Petrolandia = Viagem_Paulo_Afonso("50", "2", "63.4", "Petrolândia")
+Senhor_do_Bonfim = Viagem_Paulo_Afonso("90", "5", "397.5", "Senhor do bomfim")
+Garanhuns = Viagem_Paulo_Afonso("100", "6", "219.9", "Garanhuns")
+Serra_Talhada = Viagem_Paulo_Afonso("80", "5", "214.1", "Serra Talhada")
+Floresta = Viagem_Paulo_Afonso("85", "4", "98.2", "Floresta")
+Delmiro_Golveia = Viagem_Paulo_Afonso("40", "1.30", "37.7", "Delmiro Golveia")
+Arapiraca = Viagem_Paulo_Afonso("70", "6", "203.6", "Arapiraca")
+
+class Viagem_Gloria:
+    def __init__(self, preco, tempo, distancia, destino):
+        self.preco = preco
+        self.tempo = tempo
+        self.distancia = distancia
+        self.destino = destino
+    
+    def sua_viagem(self):
+        print(f"Você está viajando para", self.destino + "!", "Distância da viagem", self.distancia + "Km"", " "tempo da viagem", self.tempo, "horas"",", "Boa viagem!")
+    def valor_passagem(self):
+        print(f"Sua passagem para", self.destino, "custa", self.preco + "$")
+
+Aracaju = Viagem_Gloria("80", "6", "281.1", "Aracaju")
+Salvador = Viagem_Gloria("90", "8", "470.3", "Salvador")
+Recife = Viagem_Gloria("115", "9", "447.4", "Recife")
+Petrolandia = Viagem_Gloria("50", "2", "63.4", "Petrolândia")
+Senhor_do_Bonfim = Viagem_Gloria("90", "5", "397.5", "Senhor do bomfim")
+Garanhuns = Viagem_Gloria("100", "6", "219.9", "Garanhuns")
+Serra_Talhada = Viagem_Gloria("80", "5", "214.1", "Serra Talhada")
+Floresta = Viagem_Gloria("85", "4", "98.2", "Floresta")
+Delmiro_Golveia = Viagem_Gloria("40", "1.30", "37.7", "Delmiro Golveia")
+Arapiraca = Viagem_Gloria("70", "6", "203.6", "Arapiraca")
+
+class Viagem_Demiro:
+
+    def __init__(self, preco, tempo, distancia, destino):
+        self.preco = preco
+        self.tempo = tempo
+        self.distancia = distancia
+        self.destino = destino
+    
+    def sua_viagem(self):
+        print(f"Você está viajando para", self.destino + "!", "Distância da viagem", self.distancia + "Km"", " "tempo da viagem", self.tempo, "horas"",", "Boa viagem!")
+    def valor_passagem(self):
+        print(f"Sua passagem para", self.destino, "custa", self.preco + "$")
+
+Aracaju = Viagem_Demiro("80", "6", "281.1", "Aracaju")
+Salvador = Viagem_Demiro("90", "8", "470.3", "Salvador")
+Recife = Viagem_Demiro("115", "9", "447.4", "Recife")
+Petrolandia = Viagem_Demiro("50", "2", "63.4", "Petrolândia")
+Senhor_do_Bonfim = Viagem_Demiro("90", "5", "397.5", "Senhor do bomfim")
+Garanhuns = Viagem_Demiro("100", "6", "219.9", "Garanhuns")
+Serra_Talhada = Viagem_Demiro("80", "5", "214.1", "Serra Talhada")
+Floresta = Viagem_Demiro("85", "4", "98.2", "Floresta")
+Delmiro_Golveia = Viagem_Demiro("40", "1.30", "37.7", "Delmiro Golveia")
+Arapiraca = Viagem_Demiro("70", "6", "203.6", "Arapiraca")
+
+class Terminal:
+    def __init__(self, terminal):
+        self.terminal = terminal 
+
+    def Seu_terminal(self):
+        print(f"Você escolheu o terminal de", self.terminal)
+
